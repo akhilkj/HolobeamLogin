89bfba9a3113682500f5889e775d088ae6714f75
Akhil K J <akhilkj@valorem.com>
2018-04-27 13:01:53 +0530

    code drop

---
+++ HolobeamLogin/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace HolobeamLogin
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+        
+    }
+}
---
+++ HolobeamLogin/Startup.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.Owin.Security.Notifications;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.IdentityModel;
+using System.IdentityModel.Tokens;
+using Microsoft.Identity.Client;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+
+[assembly: OwinStartup(typeof(HolobeamLogin.Startup))]
+
+namespace HolobeamLogin
+{
+    public class Startup
+    {
+        //Set the API Endpoint to Graph 'me' endpoint
+        string graphAPIEndpoint = "https://graph.microsoft.com/v1.0/me";
+
+        //Set the scope for API call to user.read
+        string[] scopes = new string[] { "user.read" };
+
+        // The Client ID is used by the application to uniquely identify itself to Azure AD.
+        string clientId = System.Configuration.ConfigurationManager.AppSettings["ClientId"];
+
+        // RedirectUri is the URL where the user will be redirected to after they sign in.
+        string redirectUri = System.Configuration.ConfigurationManager.AppSettings["RedirectUri"];
+
+        // Tenant is the tenant ID (e.g. contoso.onmicrosoft.com, or 'common' for multi-tenant)
+        static string tenant = System.Configuration.ConfigurationManager.AppSettings["Tenant"];
+
+        // Authority is the URL for authority, composed by Azure Active Directory v2 endpoint and the tenant name (e.g. https://login.microsoftonline.com/contoso.onmicrosoft.com/v2.0)
+        string authority = String.Format(System.Globalization.CultureInfo.InvariantCulture, System.Configuration.ConfigurationManager.AppSettings["Authority"], tenant);
+
+        PublicClientApplication myApp = new PublicClientApplication("b1697df6-a3da-4ecb-9f43-113d6147dff1");
+        
+        /// <summary>
+        /// Configure OWIN to use OpenIdConnect 
+        /// </summary>
+        /// <param name="app"></param>
+        public void Configuration(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            app.UseOpenIdConnectAuthentication(
+            new OpenIdConnectAuthenticationOptions
+            {
+                // Sets the ClientId, authority, RedirectUri as obtained from web.config
+                ClientId = clientId,
+                Authority = authority,
+                RedirectUri = redirectUri,
+                // PostLogoutRedirectUri is the page that users will be redirected to after sign-out. In this case, it is using the home page
+                PostLogoutRedirectUri = redirectUri,
+                Scope = OpenIdConnectScope.OpenIdProfile,
+                // ResponseType is set to request the id_token - which contains basic information about the signed-in user
+                ResponseType = OpenIdConnectResponseType.IdToken,
+                // ValidateIssuer set to false to allow personal and work accounts from any organization to sign in to your application
+                // To only allow users from a single organizations, set ValidateIssuer to true and 'tenant' setting in web.config to the tenant name
+                // To allow users from only a list of specific organizations, set ValidateIssuer to true and use ValidIssuers parameter 
+                TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters() { ValidateIssuer = false },
+                // OpenIdConnectAuthenticationNotifications configures OWIN to send notification of failed authentications to OnAuthenticationFailed method
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                {
+                    AuthenticationFailed = OnAuthenticationFailed
+                }
+            });
+
+            //Microsoft.Identity.Client.AuthenticationResult result = null;
+            
+            //var a = result.AccessToken;
+
+
+
+        }
+
+        /// <summary>
+        /// Handle failed authentication requests by redirecting the user to the home page with an error in the query string
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        private Task OnAuthenticationFailed(AuthenticationFailedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> context)
+        {
+            context.HandleResponse();
+            context.Response.Redirect("/?errormessage=" + context.Exception.Message);
+            return Task.FromResult(0);
+        }
+
+    }
+}
